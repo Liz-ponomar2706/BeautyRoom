5cc9c761e98448e13b48c9dd23376771d28a37b7
Liz-ponomar2706 <129830080+Liz-ponomar2706@users.noreply.github.com>
2023-04-16 19:12:48 +0300

    Вход\Регистрация пользователя

--- BeautyRoom/Form1.Designer.cs
+++ BeautyRoom/Form1.Designer.cs
@@ -30,15 +30,15 @@ namespace BeautyRoom
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.gunaPictureBox1 = new Guna.UI.WinForms.GunaPictureBox();
-            this.gunaTextBox2 = new Guna.UI.WinForms.GunaTextBox();
-            this.gunaTextBox3 = new Guna.UI.WinForms.GunaTextBox();
+            this.NumberTextBox = new Guna.UI.WinForms.GunaTextBox();
+            this.PasswordTextBox = new Guna.UI.WinForms.GunaTextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.gunaPictureBox2 = new Guna.UI.WinForms.GunaPictureBox();
-            this.gunaButton1 = new Guna.UI.WinForms.GunaButton();
+            this.LoginButton = new Guna.UI.WinForms.GunaButton();
             this.gunaCheckBox1 = new Guna.UI.WinForms.GunaCheckBox();
-            this.gunaButton2 = new Guna.UI.WinForms.GunaButton();
-            this.gunaTextBox1 = new Guna.UI.WinForms.GunaTextBox();
+            this.SignUpButton = new Guna.UI.WinForms.GunaButton();
+            this.NameTextBox = new Guna.UI.WinForms.GunaTextBox();
             ((System.ComponentModel.ISupportInitialize)(this.gunaPictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.gunaPictureBox2)).BeginInit();
             this.SuspendLayout();
@@ -54,39 +54,39 @@ namespace BeautyRoom
             this.gunaPictureBox1.TabIndex = 0;
             this.gunaPictureBox1.TabStop = false;
             // 
-            // gunaTextBox2
+            // NumberTextBox
             // 
-            this.gunaTextBox2.BaseColor = System.Drawing.Color.White;
-            this.gunaTextBox2.BorderColor = System.Drawing.Color.Silver;
-            this.gunaTextBox2.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.gunaTextBox2.FocusedBaseColor = System.Drawing.Color.White;
-            this.gunaTextBox2.FocusedBorderColor = System.Drawing.Color.Maroon;
-            this.gunaTextBox2.FocusedForeColor = System.Drawing.SystemColors.ControlText;
-            this.gunaTextBox2.Font = new System.Drawing.Font("Segoe UI", 9F);
-            this.gunaTextBox2.Location = new System.Drawing.Point(67, 230);
-            this.gunaTextBox2.Name = "gunaTextBox2";
-            this.gunaTextBox2.PasswordChar = '\0';
-            this.gunaTextBox2.SelectedText = "";
-            this.gunaTextBox2.Size = new System.Drawing.Size(250, 35);
-            this.gunaTextBox2.TabIndex = 1;
-            this.gunaTextBox2.Text = "Номер телефона";
+            this.NumberTextBox.BaseColor = System.Drawing.Color.White;
+            this.NumberTextBox.BorderColor = System.Drawing.Color.Silver;
+            this.NumberTextBox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.NumberTextBox.FocusedBaseColor = System.Drawing.Color.White;
+            this.NumberTextBox.FocusedBorderColor = System.Drawing.Color.Maroon;
+            this.NumberTextBox.FocusedForeColor = System.Drawing.SystemColors.ControlText;
+            this.NumberTextBox.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.NumberTextBox.Location = new System.Drawing.Point(67, 230);
+            this.NumberTextBox.Name = "NumberTextBox";
+            this.NumberTextBox.PasswordChar = '\0';
+            this.NumberTextBox.SelectedText = "";
+            this.NumberTextBox.Size = new System.Drawing.Size(250, 35);
+            this.NumberTextBox.TabIndex = 1;
+            this.NumberTextBox.Text = "Номер телефона";
             // 
-            // gunaTextBox3
+            // PasswordTextBox
             // 
-            this.gunaTextBox3.BaseColor = System.Drawing.Color.White;
-            this.gunaTextBox3.BorderColor = System.Drawing.Color.Silver;
-            this.gunaTextBox3.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.gunaTextBox3.FocusedBaseColor = System.Drawing.Color.White;
-            this.gunaTextBox3.FocusedBorderColor = System.Drawing.Color.Maroon;
-            this.gunaTextBox3.FocusedForeColor = System.Drawing.SystemColors.ControlText;
-            this.gunaTextBox3.Font = new System.Drawing.Font("Segoe UI", 9F);
-            this.gunaTextBox3.Location = new System.Drawing.Point(67, 285);
-            this.gunaTextBox3.Name = "gunaTextBox3";
-            this.gunaTextBox3.PasswordChar = '\0';
-            this.gunaTextBox3.SelectedText = "";
-            this.gunaTextBox3.Size = new System.Drawing.Size(250, 35);
-            this.gunaTextBox3.TabIndex = 2;
-            this.gunaTextBox3.Text = "Пароль";
+            this.PasswordTextBox.BaseColor = System.Drawing.Color.White;
+            this.PasswordTextBox.BorderColor = System.Drawing.Color.Silver;
+            this.PasswordTextBox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.PasswordTextBox.FocusedBaseColor = System.Drawing.Color.White;
+            this.PasswordTextBox.FocusedBorderColor = System.Drawing.Color.Maroon;
+            this.PasswordTextBox.FocusedForeColor = System.Drawing.SystemColors.ControlText;
+            this.PasswordTextBox.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.PasswordTextBox.Location = new System.Drawing.Point(67, 285);
+            this.PasswordTextBox.Name = "PasswordTextBox";
+            this.PasswordTextBox.PasswordChar = '\0';
+            this.PasswordTextBox.SelectedText = "";
+            this.PasswordTextBox.Size = new System.Drawing.Size(250, 35);
+            this.PasswordTextBox.TabIndex = 2;
+            this.PasswordTextBox.Text = "Пароль";
             // 
             // label1
             // 
@@ -120,29 +120,29 @@ namespace BeautyRoom
             this.gunaPictureBox2.TabIndex = 5;
             this.gunaPictureBox2.TabStop = false;
             // 
-            // gunaButton1
+            // LoginButton
             // 
-            this.gunaButton1.AnimationHoverSpeed = 0.07F;
-            this.gunaButton1.AnimationSpeed = 0.03F;
-            this.gunaButton1.BaseColor = System.Drawing.Color.Maroon;
-            this.gunaButton1.BorderColor = System.Drawing.Color.Black;
-            this.gunaButton1.DialogResult = System.Windows.Forms.DialogResult.None;
-            this.gunaButton1.FocusedColor = System.Drawing.Color.Empty;
-            this.gunaButton1.Font = new System.Drawing.Font("Segoe UI", 9F);
-            this.gunaButton1.ForeColor = System.Drawing.Color.Snow;
-            this.gunaButton1.Image = null;
-            this.gunaButton1.ImageSize = new System.Drawing.Size(20, 20);
-            this.gunaButton1.Location = new System.Drawing.Point(100, 381);
-            this.gunaButton1.Name = "gunaButton1";
-            this.gunaButton1.OnHoverBaseColor = System.Drawing.Color.Maroon;
-            this.gunaButton1.OnHoverBorderColor = System.Drawing.Color.Black;
-            this.gunaButton1.OnHoverForeColor = System.Drawing.Color.White;
-            this.gunaButton1.OnHoverImage = null;
-            this.gunaButton1.OnPressedColor = System.Drawing.Color.Black;
-            this.gunaButton1.Size = new System.Drawing.Size(125, 45);
-            this.gunaButton1.TabIndex = 8;
-            this.gunaButton1.Text = "       Войти";
-            this.gunaButton1.Click += new System.EventHandler(this.gunaButton1_Click);
+            this.LoginButton.AnimationHoverSpeed = 0.07F;
+            this.LoginButton.AnimationSpeed = 0.03F;
+            this.LoginButton.BaseColor = System.Drawing.Color.Maroon;
+            this.LoginButton.BorderColor = System.Drawing.Color.Black;
+            this.LoginButton.DialogResult = System.Windows.Forms.DialogResult.None;
+            this.LoginButton.FocusedColor = System.Drawing.Color.Empty;
+            this.LoginButton.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.LoginButton.ForeColor = System.Drawing.Color.Snow;
+            this.LoginButton.Image = null;
+            this.LoginButton.ImageSize = new System.Drawing.Size(20, 20);
+            this.LoginButton.Location = new System.Drawing.Point(100, 381);
+            this.LoginButton.Name = "LoginButton";
+            this.LoginButton.OnHoverBaseColor = System.Drawing.Color.Maroon;
+            this.LoginButton.OnHoverBorderColor = System.Drawing.Color.Black;
+            this.LoginButton.OnHoverForeColor = System.Drawing.Color.White;
+            this.LoginButton.OnHoverImage = null;
+            this.LoginButton.OnPressedColor = System.Drawing.Color.Black;
+            this.LoginButton.Size = new System.Drawing.Size(125, 45);
+            this.LoginButton.TabIndex = 8;
+            this.LoginButton.Text = "       Войти";
+            this.LoginButton.Click += new System.EventHandler(this.gunaButton1_Click);
             // 
             // gunaCheckBox1
             // 
@@ -156,45 +156,47 @@ namespace BeautyRoom
             this.gunaCheckBox1.TabIndex = 9;
             this.gunaCheckBox1.Text = "Роль администратора";
             // 
-            // gunaButton2
+            // SignUpButton
             // 
-            this.gunaButton2.AnimationHoverSpeed = 0.07F;
-            this.gunaButton2.AnimationSpeed = 0.03F;
-            this.gunaButton2.BaseColor = System.Drawing.Color.Maroon;
-            this.gunaButton2.BorderColor = System.Drawing.Color.Black;
-            this.gunaButton2.DialogResult = System.Windows.Forms.DialogResult.None;
-            this.gunaButton2.FocusedColor = System.Drawing.Color.Empty;
-            this.gunaButton2.Font = new System.Drawing.Font("Segoe UI", 9F);
-            this.gunaButton2.ForeColor = System.Drawing.Color.Snow;
-            this.gunaButton2.Image = null;
-            this.gunaButton2.ImageSize = new System.Drawing.Size(20, 20);
-            this.gunaButton2.Location = new System.Drawing.Point(47, 443);
-            this.gunaButton2.Name = "gunaButton2";
-            this.gunaButton2.OnHoverBaseColor = System.Drawing.Color.Maroon;
-            this.gunaButton2.OnHoverBorderColor = System.Drawing.Color.Black;
-            this.gunaButton2.OnHoverForeColor = System.Drawing.Color.White;
-            this.gunaButton2.OnHoverImage = null;
-            this.gunaButton2.OnPressedColor = System.Drawing.Color.Black;
-            this.gunaButton2.Size = new System.Drawing.Size(270, 45);
-            this.gunaButton2.TabIndex = 10;
-            this.gunaButton2.Text = " Нет аккуанта? Зарегистрируйтесь";
+            this.SignUpButton.AnimationHoverSpeed = 0.07F;
+            this.SignUpButton.AnimationSpeed = 0.03F;
+            this.SignUpButton.BaseColor = System.Drawing.Color.Maroon;
+            this.SignUpButton.BorderColor = System.Drawing.Color.Black;
+            this.SignUpButton.DialogResult = System.Windows.Forms.DialogResult.None;
+            this.SignUpButton.FocusedColor = System.Drawing.Color.Empty;
+            this.SignUpButton.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.SignUpButton.ForeColor = System.Drawing.Color.Snow;
+            this.SignUpButton.Image = null;
+            this.SignUpButton.ImageSize = new System.Drawing.Size(20, 20);
+            this.SignUpButton.Location = new System.Drawing.Point(47, 443);
+            this.SignUpButton.Name = "SignUpButton";
+            this.SignUpButton.OnHoverBaseColor = System.Drawing.Color.Maroon;
+            this.SignUpButton.OnHoverBorderColor = System.Drawing.Color.Black;
+            this.SignUpButton.OnHoverForeColor = System.Drawing.Color.White;
+            this.SignUpButton.OnHoverImage = null;
+            this.SignUpButton.OnPressedColor = System.Drawing.Color.Black;
+            this.SignUpButton.Size = new System.Drawing.Size(270, 45);
+            this.SignUpButton.TabIndex = 10;
+            this.SignUpButton.Text = " Нет аккуанта? Зарегистрируйтесь";
+            this.SignUpButton.Click += new System.EventHandler(this.SignUpButton_Click);
             // 
-            // gunaTextBox1
+            // NameTextBox
             // 
-            this.gunaTextBox1.BaseColor = System.Drawing.Color.White;
-            this.gunaTextBox1.BorderColor = System.Drawing.Color.Silver;
-            this.gunaTextBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.gunaTextBox1.FocusedBaseColor = System.Drawing.Color.White;
-            this.gunaTextBox1.FocusedBorderColor = System.Drawing.Color.Maroon;
-            this.gunaTextBox1.FocusedForeColor = System.Drawing.SystemColors.ControlText;
-            this.gunaTextBox1.Font = new System.Drawing.Font("Segoe UI", 9F);
-            this.gunaTextBox1.Location = new System.Drawing.Point(67, 178);
-            this.gunaTextBox1.Name = "gunaTextBox1";
-            this.gunaTextBox1.PasswordChar = '\0';
-            this.gunaTextBox1.SelectedText = "";
-            this.gunaTextBox1.Size = new System.Drawing.Size(250, 35);
-            this.gunaTextBox1.TabIndex = 11;
-            this.gunaTextBox1.Text = "Имя";
+            this.NameTextBox.BaseColor = System.Drawing.Color.White;
+            this.NameTextBox.BorderColor = System.Drawing.Color.Silver;
+            this.NameTextBox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.NameTextBox.FocusedBaseColor = System.Drawing.Color.White;
+            this.NameTextBox.FocusedBorderColor = System.Drawing.Color.Maroon;
+            this.NameTextBox.FocusedForeColor = System.Drawing.SystemColors.ControlText;
+            this.NameTextBox.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.NameTextBox.Location = new System.Drawing.Point(67, 178);
+            this.NameTextBox.Name = "NameTextBox";
+            this.NameTextBox.PasswordChar = '\0';
+            this.NameTextBox.SelectedText = "";
+            this.NameTextBox.Size = new System.Drawing.Size(250, 35);
+            this.NameTextBox.TabIndex = 11;
+            this.NameTextBox.Text = "Имя";
+            this.NameTextBox.TextChanged += new System.EventHandler(this.gunaTextBox1_TextChanged);
             // 
             // Form1
             // 
@@ -202,15 +204,15 @@ namespace BeautyRoom
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.WhiteSmoke;
             this.ClientSize = new System.Drawing.Size(950, 519);
-            this.Controls.Add(this.gunaTextBox1);
-            this.Controls.Add(this.gunaButton2);
+            this.Controls.Add(this.NameTextBox);
+            this.Controls.Add(this.SignUpButton);
             this.Controls.Add(this.gunaCheckBox1);
-            this.Controls.Add(this.gunaButton1);
+            this.Controls.Add(this.LoginButton);
             this.Controls.Add(this.gunaPictureBox2);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.gunaTextBox3);
-            this.Controls.Add(this.gunaTextBox2);
+            this.Controls.Add(this.PasswordTextBox);
+            this.Controls.Add(this.NumberTextBox);
             this.Controls.Add(this.gunaPictureBox1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
@@ -228,15 +230,15 @@ namespace BeautyRoom
         #endregion
 
         private Guna.UI.WinForms.GunaPictureBox gunaPictureBox1;
-        private Guna.UI.WinForms.GunaTextBox gunaTextBox2;
-        private Guna.UI.WinForms.GunaTextBox gunaTextBox3;
+        private Guna.UI.WinForms.GunaTextBox NumberTextBox;
+        private Guna.UI.WinForms.GunaTextBox PasswordTextBox;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private Guna.UI.WinForms.GunaPictureBox gunaPictureBox2;
-        private Guna.UI.WinForms.GunaButton gunaButton1;
+        private Guna.UI.WinForms.GunaButton LoginButton;
         private Guna.UI.WinForms.GunaCheckBox gunaCheckBox1;
-        private Guna.UI.WinForms.GunaButton gunaButton2;
-        private Guna.UI.WinForms.GunaTextBox gunaTextBox1;
+        private Guna.UI.WinForms.GunaButton SignUpButton;
+        private Guna.UI.WinForms.GunaTextBox NameTextBox;
     }
 }
 
--- BeautyRoom/Form1.cs
+++ BeautyRoom/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,25 +19,57 @@ namespace BeautyRoom
             mainWindow = new MainWindow();
         }
         MainWindow mainWindow;
-        
-        string User = "Мария";
-        string PhoneNumber = "89012345678";
-        string Password = "123456";
+
+        string path = @"D:\Дз_Программы\BeautyRoom\User.txt";
         private void gunaButton1_Click(object sender, EventArgs e)
         {
-            string UserName = gunaTextBox1.Text;
-            string Number = gunaTextBox2.Text;
-            string Pas = gunaTextBox3.Text;
-            if (UserName == User && Number == PhoneNumber && Pas == Password)
+            string name = NameTextBox.Text;
+            string number = NumberTextBox.Text;
+            string password = PasswordTextBox.Text;
+            bool correctName = false;
+            bool correctNumber = false;
+            bool correctPassword = false;
+
+            string[] line = File.ReadAllLines(path);
+            for (int i = 0; i < line.Length; i++)
             {
-                mainWindow.Show();
+                if (name == line[i])
+                {
+                    correctName = true;
+                }
+                if (number == line[i])
+                {
+                    correctNumber = true;
+                }
+                if (password == line[i])
+                {
+                    correctPassword = true;
+                }
+
+            }
+            if (correctName == true && correctNumber == true && correctPassword == true)
+            {
+                mainWindow.Show(); ;
             }
             else
             {
-                MessageBox.Show("Ошибка в логине или пароле.Попробуйте снова!","Ошибка авторизации");
-
+                MessageBox.Show("Ошибка! Проверьте введные данные!","Ошибка авторизации");
             }
         }
 
+        private void gunaTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SignUpButton_Click(object sender, EventArgs e)
+        {
+            using (StreamWriter stream = new StreamWriter(path, true))
+            {
+                stream.WriteLine(NameTextBox.Text);
+                stream.WriteLine(NumberTextBox.Text);
+                stream.WriteLine(PasswordTextBox.Text);
+            }
+        }
     }
 }
